96895c0428e25130271bdce1346eb40c049fd3b4
Jacob <73400909+Infinium-dev@users.noreply.github.com>
2020-11-14 23:42:38 +0100

    Add infinium prefix

--- variant.go
+++ variant.go
@@ -1,8 +1,9 @@
 package vanity
 
-type Network []byte
+type Network []uint32
 
 var (
+	InfiniumMainNetwork = Network{0x544aa79}
 	MoneroMainNetwork = Network{0x12}
 	MoneroTestNetwork = Network{0x35}
 	GraftMainNetwork  = Network{0x5a}
